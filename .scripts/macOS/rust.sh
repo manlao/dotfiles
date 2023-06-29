463fd3ea15cd923c2f11919226a677f31f47251d
Sean Tu <manlao@gmail.com>
2023-06-29 17:24:13 +0800

    feat: add rust.sh

---
+++ .scripts/macOS/rust.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1091
+source "$DOTFILES_HOME/trait.rc"
+
+install() {
+  install_rust
+}
+
+install_rust() {
+  if ! brew list rustup-init 1>/dev/null 2>&1; then
+    message --info "Install rustup"
+    brew install rustup-init
+    rustup-init -y
+  fi
+}
+
+update() {
+  update_rust
+}
+
+update_rust() {
+  rustup update
+}
+
+main "$@"

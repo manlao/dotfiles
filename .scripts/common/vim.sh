99f7d37a26f65ab92777b12b7cc8aa53c8ccb771
Sean Tu <manlao@gmail.com>
2024-04-15 15:00:09 +0800

    fix: pip permission issue

--- .scripts/common/vim.sh
+++ .scripts/common/vim.sh
@@ -33,7 +33,7 @@ install_vim() {
 
 install_vim_dependencies() {
   message --info "Install vim dependencies"
-  "$(brew --prefix "$(brew deps vim | grep python)")/libexec/bin/pip" install "${DEPS[@]}" --break-system-packages --no-warn-script-location
+  "$(brew --prefix "$(brew deps vim | grep python)")/libexec/bin/pip" install "${DEPS[@]}" --user --break-system-packages --no-warn-script-location
 }
 
 install_neovim() {
@@ -49,7 +49,7 @@ install_neovim() {
 
 install_neovim_dependencies() {
   message --info "Install neovim dependencies"
-  "$(brew --prefix python)/libexec/bin/pip" install "${DEPS[@]}" --break-system-packages --no-warn-script-location
+  "$(brew --prefix python)/libexec/bin/pip" install "${DEPS[@]}" --user --break-system-packages --no-warn-script-location
 }
 
 install_vim_plug() {
@@ -110,7 +110,7 @@ update_vim_dependencies() {
 
   local PIP
   PIP="$(brew --prefix "$(brew deps vim | grep python)")/libexec/bin/pip"
-  "$PIP" --disable-pip-version-check list | tail -n +3 | cut -f 1 -d ' ' | xargs "$PIP" install --break-system-packages --upgrade --no-warn-script-location
+  "$PIP" --disable-pip-version-check list | tail -n +3 | cut -f 1 -d ' ' | xargs "$PIP" install --user --break-system-packages --upgrade --no-warn-script-location
 }
 
 update_neovim_dependencies() {
@@ -118,7 +118,7 @@ update_neovim_dependencies() {
 
   local PIP
   PIP="$(brew --prefix python)/libexec/bin/pip"
-  "$PIP" --disable-pip-version-check list | tail -n +3 | cut -f 1 -d ' ' | xargs "$PIP" install --break-system-packages --upgrade --no-warn-script-location
+  "$PIP" --disable-pip-version-check list | tail -n +3 | cut -f 1 -d ' ' | xargs "$PIP" install --user --break-system-packages --upgrade --no-warn-script-location
 }
 
 update_vim_plug_plugins() {
